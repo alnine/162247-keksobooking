d07a6859152f7c4146e97e62a37f3a59f2bb737e
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2018-11-23 19:01:56 +0200

    Добавляет функцию для генерации одного объекта, который хранит описание предложения

--- js/map.js
+++ js/map.js
@@ -28,3 +28,34 @@ function getRandomIntegerFromInterval(min, max) {
 function getRandomElementFromArray(arr) {
   return arr[getRandomIntegerFromInterval(0, arr.length - 1)];
 }
+
+function createOffer(count) {
+  var data = {
+    'author': {
+      'avatar': 'img/avatars/user0' + (count + 1) + '.png'
+    },
+
+    'offer': {
+      'title': TITLES[count],
+      'address': '',
+      'price': getRandomIntegerFromInterval(1000, 1000000),
+      'type': getRandomElementFromArray(TYPES),
+      'rooms': getRandomIntegerFromInterval(1, 5),
+      'guests': getRandomIntegerFromInterval(1, 8),
+      'checkin': getRandomElementFromArray(TIMEFRAMES),
+      'checkout': getRandomElementFromArray(TIMEFRAMES),
+      'features': FEATURES.slice(getRandomIntegerFromInterval(-(FEATURES.length - 1), FEATURES.length - 1)),
+      'description': '',
+      'photos': PHOTOS
+    },
+
+    'location': {
+      'x': getRandomIntegerFromInterval(PIN_WIDTH / 2, MAP_WIDTH - PIN_WIDTH / 2),
+      'y': getRandomIntegerFromInterval(130, 630)
+    }
+  };
+
+  data.offer.address = data.location.x + ', ' + data.location.y;
+
+  return data;
+}
