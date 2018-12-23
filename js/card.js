389b6822c497e091e8ae2280c2ca022309110a43
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2018-12-23 14:13:11 +0200

    Исправляет Д25: длинная функция разбита на несколько небольших

--- js/card.js
+++ js/card.js
@@ -47,78 +47,55 @@
     }
   }
 
+  function fillLayout(element, data) {
+    var featuresList = element.querySelector('.popup__features');
+    var photosList = element.querySelector('.popup__photos');
+    var photoTemplate = photosList.querySelector('img');
+
+    element.querySelector('.popup__avatar').src = data.author.avatar;
+    element.querySelector('.popup__title').textContent = data.offer.title;
+    element.querySelector('.popup__text--address').textContent = data.offer.address;
+    element.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
+    element.querySelector('.popup__type').textContent = TypesLabel[data.offer.type];
+    element.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+    element.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+    element.querySelector('.popup__description').textContent = data.offer.description;
+
+    featuresList.innerHTML = '';
+    featuresList.appendChild(getFeaturesLayout(data.offer.features));
+    photosList.innerHTML = '';
+    photosList.appendChild(getPhotosLayout(data.offer.photos, photoTemplate));
+  }
+
+  function getEmptyElementChilds(childs) {
+    var emptyChilds = childs.filter(function (child, index) {
+      if (index > 1 && child.innerHTML === '') {
+        return true;
+      }
+      return false;
+    });
+
+    return emptyChilds;
+  }
+
+  function deleteEmptyElementChilds(parent) {
+    var parentChilds = Array.from(parent.children);
+    var emptyChilds = getEmptyElementChilds(parentChilds);
+    emptyChilds.forEach(function (child) {
+      parent.removeChild(child);
+    });
+  }
+
   function getLayout(data) {
     var layoutItem = templateElement.cloneNode(true);
-    var featuresList = layoutItem.querySelector('.popup__features');
-    var photosList = layoutItem.querySelector('.popup__photos');
-    var photoTemplate = photosList.querySelector('img');
     var closeButton = layoutItem.querySelector('.popup__close');
+
+    fillLayout(layoutItem, data);
+    deleteEmptyElementChilds(layoutItem);
 
     closeButton.addEventListener('click', function () {
       close();
     });
-
-    if (data.offer.title) {
-      layoutItem.querySelector('.popup__title').textContent = data.offer.title;
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__title'));
-    }
-
-    if (data.offer.address) {
-      layoutItem.querySelector('.popup__text--address').textContent = data.offer.address;
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__text--address'));
-    }
-
-    if (data.offer.price) {
-      layoutItem.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__text--price'));
-    }
-
-    if (data.offer.type) {
-      layoutItem.querySelector('.popup__type').textContent = TypesLabel[data.offer.type];
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__type'));
-    }
-
-    if (data.offer.rooms && data.offer.guests) {
-      layoutItem.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__text--capacity'));
-    }
-
-    if (data.offer.checkin && data.offer.checkout) {
-      layoutItem.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__text--time'));
-    }
-
-    if (data.offer.features) {
-      featuresList.innerHTML = '';
-      featuresList.appendChild(getFeaturesLayout(data.offer.features));
-    } else {
-      layoutItem.removeChild(featuresList);
-    }
-
-    if (data.offer.description) {
-      layoutItem.querySelector('.popup__description').textContent = data.offer.description;
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__description'));
-    }
-
-    if (data.offer.photos) {
-      photosList.innerHTML = '';
-      photosList.appendChild(getPhotosLayout(data.offer.photos, photoTemplate));
-    } else {
-      layoutItem.removeChild(photosList);
-    }
-
-    if (data.author.avatar) {
-      layoutItem.querySelector('.popup__avatar').src = data.author.avatar;
-    } else {
-      layoutItem.removeChild(layoutItem.querySelector('.popup__avatar'));
-    }
 
     return layoutItem;
   }
